e9962879274eb165790cddd965a6c0d963ba8a9c
nicosesma <nicosm310@gmail.com>
2017-09-09 16:01:50 -0700

    Setup for Voting Contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,5 +3,39 @@ pragma solidity ^0.4.2;
 import "./SimpleStorage.sol";
 
 contract Voting is SimpleStorage {
+  struct ContentId {
+    bytes32 content;
+  }
 
+  mapping (ContentId => address) private contentMap;
+
+  function upVote (contentId) {
+
+  }
+
+  function getVotes(contentId) {
+
+  }
+
+  function createContent(bytes32 contentId) payable returns (bytes32) {
+    // Check if user exists.
+    // If yes, return user name.
+    // If no, check if name was sent.
+    // If yes, create and return user.
+    // If no, throw.
+
+    if (contentId == 0x0)
+    {
+        throw;
+    }
+
+    if (users[msg.sender].contentId == 0x0)
+    {
+        users[msg.sender].contentId = contentId;
+
+        return (users[msg.sender].contentId);
+    }
+
+    return (users[msg.sender].contentId);
+  }
 }
