c4125dfa26efce857bbd40f1f10d778bbbf46dc5
pmuramalla <praveen.muramalla@gmail.com>
2017-09-23 16:24:12 -0700

    Update Voting.sol
    Added Title  
    Did the Transfer token

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.2;
 
+import './FixedSupplyToken.sol';
+
 contract Voting {
   //TODO create unit tests, that not only test, but guid a javascript / UI dev how to use this.
   //TODO figure out how to get a list of URLS back to client, given a user address
@@ -10,24 +12,60 @@ contract Voting {
     uint voteCount; //total upvotes
   }
 
+
+  //struct Content {
+  //  string url;
+  //  string contentTitle;
+  //}
+
+
+  // Key is the URL 
+  mapping (string => string) urlToTitle;
+
   // maps URL to Vote struct
   mapping (string => Vote) private voteMap;
 
-  // maps userID (account address) to content
-  mapping (address => string) private curatorMap;
+  //mapping (string => Content) private content;
 
+  // maps userID (account address) to content ( URL )
+  // changed to facilitate the idea:
+  // A user can upload multiple contents 
+  // and each content is a URL
+  mapping (string => address) private curatorMap;
+  
+  FixedSupplyToken fsToken = new FixedSupplyToken();
+
+
+  //     function transfer(address _to, uint256 _amount) returns (bool success) {
+
+  
+  
+  
   function upVote (string url) {
+
     voteMap[url].voteCount++;
+
+    address curatorsAddress = curatorMap[url];
+
+    bool transferSuccess = fsToken.transfer(curatorsAddress, 1);
+    if (!transferSuccess) {
+      throw;
+    }
+
   }
 
   function getVotes(string url) returns(uint count) {
     return voteMap[url].voteCount;
   }
 
-  function createContent(string url) {
+  function createContent(string url, string title) {
     require (voteMap[url].curator == 0x0); //entry should not exist
     voteMap[url].curator = msg.sender;
     voteMap[url].voteCount = 1;
-    curatorMap[msg.sender] = url;
+    urlToTitle[url] = title;
+    curatorMap[url] = msg.sender;
+
+
   }
+
 }
